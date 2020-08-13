48b59e2b09de2927c26f9b9578d83513e95abb76
Saptarshi Hore <saptarshi@pop-os.localdomain>
2020-08-14 02:47:22 +0530

    DB setup and listings app

--- btre/urls.py
+++ btre/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('listings/',include('listings.urls')),
+    path('admin/', admin.site.urls),
 ]
