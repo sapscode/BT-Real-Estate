8fd28ad4d883dbe3833f8bb33cd19b7329782341
Saptarshi Hore <saptarshi@pop-os.localdomain>
2020-08-28 03:53:12 +0530

    Search setup

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 # Create your views here.
@@ -16,7 +17,52 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html ')
+    query_set = Listing.objects.order_by('-list_date')
+
+    #keywords
+    if 'keywords' in request.GET: # if keyword exist in the request data, ( .GET looks for the name attribute)
+        keywords = request.GET['keywords'] #then put the keyword data in the keyword variable
+        if keywords: #to check it is not a empty string
+            query_set = query_set.filter(description__icontains=keywords) # __icontains is used to match if it contains text, not have to be exact
+
+    #city 
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_set = query_set.filter(city__iexact=city) # __iexact is used to match if it contains that exact text, 'i' is for case insensitive
+
+    #state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_set = query_set.filter(state__iexact=state)
+
+    #bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_set = query_set.filter(bedrooms__lte=bedrooms) #lte = less than or equal
+
+    #price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_set = query_set.filter(price__lte=price)
+
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+        'listings': query_set,
+        'values': request.GET # whatever we search for will be available to the search page 
+    }
+
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
+
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings' : listings,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    mvp_realtor = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtor': mvp_realtor
+    }
+
+    return render(request, 'pages/about.html', context)
