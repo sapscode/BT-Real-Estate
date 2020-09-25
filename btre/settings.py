6455d0a8e774af730eaf5d917ce9efba11a235fe
Saptarshi Hore <saptarshi@pop-os.localdomain>
2020-09-26 00:03:35 +0530

    n

--- btre/settings.py
+++ btre/settings.py
@@ -166,4 +166,4 @@ AWS_STORAGE_BUCKET_NAME = 'saps-btre'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
